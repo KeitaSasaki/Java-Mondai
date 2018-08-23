a599fab0fa25b44990fec7e8f246d8258d97d563
KeitaSasaki <41316431+KeitaSasaki@users.noreply.github.com>
2018-08-23 14:51:22 +0900

    TsuukaTest_4
    This reverts commit 80afe54e5caa7f2b2c26e318a71b57597cef2b96.

---
+++ TsuukaTest_4/src/Program.java
@@ -0,0 +1,15 @@
+
+public class Program {
+	public static void main(String[] args) {
+		int n = 10;
+		double x = 2.5;
+		System.out.println(n + "/" + 3);
+		//結果:10/3
+		System.out.println(n + 1 + "/" +3);
+		//結果:11/3
+		System.out.println("number is " + x + n);
+		//結果:number is 2.510
+		System.out.println("number is " + (x + n));
+		//結果:number is 12.5
+	}
+}
