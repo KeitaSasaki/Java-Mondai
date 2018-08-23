c7dada14767b8923353b5b6da5d75168dcb48800
KeitaSasaki <41316431+KeitaSasaki@users.noreply.github.com>
2018-08-23 14:57:09 +0900

    TsuukaTest_5

---
+++ TsuukaTest_5/src/Program.java
@@ -0,0 +1,11 @@
+
+public class Program {
+	public static void main(String[] args) {
+		int a,b,c;
+		b=c=1;
+		a=b+=c++;
+		System.out.println("a=" + a + "b=" + b +"C=" + c);
+		//結果a=2b=2C=2
+
+	}
+}
