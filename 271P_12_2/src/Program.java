3d79c0b516a2ad3f09254f4a3579d0d9dcb4f496
KeitaSasaki <41316431+KeitaSasaki@users.noreply.github.com>
2018-08-28 17:24:54 +0900

    メソッドを分けた

--- 271P_12_2/src/Program.java
+++ 271P_12_2/src/Program.java
@@ -1,21 +1,28 @@
 import java.util.Scanner;
 
 public class Program {
-	public static void main(String[] args) {
-		greet();
+	public static void main(String args[]) {
+		String name = scanLine("名前を入力してください");
+		String number= scanLine("続いて性別を[半角]入力してください。\\r\\n女性:0\\r\\n男性:1");
+		syori(name, number);
 	}
-	public static void greet(){
+
+	//入力
+	private static String scanLine(String title){
+		System.out.println(title);
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("名前を入力してください。");
-		String Name = scanner.next();
-		System.out.println("名前が入力されました。\r\n続いて性別を数字で入力してください\r\n----------\r\n女性:0\r\n男性:1\r\n----------");
-		int	nomber = scanner.nextInt();
-		switch (nomber) {
-		case 0:
-			System.out.println("こんにちは"+Name+"さん");
+		String input = scanner.next();
+		return input;
+	}
+	//処理
+	public static void syori(String name,String number)
+	{
+		switch (number) {
+		case "0":
+			System.out.println("こんにちは"+name+"さん");
 			break;
-		case 1:
-			System.out.println("こんにちは"+Name+"くん");
+		case "1":
+			System.out.println("こんにちは"+name+"くん");
 			break;
 		default:
 			System.out.println("(^q^)くおえうえーーーるえうおおおｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗ");
---
+++ test1/src/test1/Program.java
@@ -0,0 +1,15 @@
+package test1;
+
+public class Program {
+	public static void main(String[] args) {
+		String name = "ああ";
+		scanLine(name);
+	}
+
+	public static void scanLine(String name) {
+		System.out.println(name);
+	}
+
+	public static void student() {
+	}
+}
